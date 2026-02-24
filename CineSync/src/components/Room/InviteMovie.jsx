31482e50581e92ad67ff2473f7e6430a1cf08e09
Harivigneshr57 <harivigneshr57@gmail.com>
2026-02-24 10:57:39 +0530

    changes

--- CineSync/src/components/Room/InviteMovie.jsx
+++ CineSync/src/components/Room/InviteMovie.jsx
@@ -18,7 +18,7 @@ export default function InviteMovie({ code, onStep, step, movie, room }) {
         setRoomVideo(movie);
         setMovieName(title);
         localStorage.setItem("MovieName",title)
-        localStorage.setItem('movieVideo');
+        localStorage.setItem('movieVideo',video);
         setRoomName(localStorage.getItem('Roomname'));
         navigateToMainRoom()
     }

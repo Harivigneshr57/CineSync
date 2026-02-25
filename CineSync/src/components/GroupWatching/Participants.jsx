e94544d001fc53c22d899eefd0397ed72d7161bd
Harivigneshr57 <harivigneshr57@gmail.com>
2026-02-25 14:42:38 +0530

    Fixed COnflict

--- CineSync/src/components/GroupWatching/Participants.jsx
+++ CineSync/src/components/GroupWatching/Participants.jsx
@@ -247,14 +247,7 @@ export default function Participants({ roomName, username, micOn, setMicOn, camO
         <div className="personDetails">
             <h6>FRIEND</h6>
             <h5>{micOn?'Click to Mute':'Click to Unmute'}</h5> 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            {ref.current.muted?<i className="fa-solid fa-microphone"></i>:<i class="fa-solid fa-microphone-slash"></i>}
->>>>>>> parent of 48e43ea (remove render yaml)
-=======
-            {ref.current.muted?<i className="fa-solid fa-microphone"></i>:<i class="fa-solid fa-microphone-slash"></i>}
->>>>>>> parent of 48e43ea (remove render yaml)
+            {/* {ref.current.muted?<i className="fa-solid fa-microphone"></i>:<i class="fa-solid fa-microphone-slash"></i>} */}
         </div>
 
             {/* <button onClick={() => toggleUserMute(user.id)}>
